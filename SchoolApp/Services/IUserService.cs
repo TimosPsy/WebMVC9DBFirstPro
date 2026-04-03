ebd5fbcdc932e8e44e4c612beceaa4d621692cc8
Timos <psychoson91@gmail.com>
2026-04-03 16:58:41 +0300

    chore(services): add user, teacher and student serice contracts and impls

---
+++ SchoolApp/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using SchoolApp.Core;
+using SchoolApp.Core.Filters;
+using SchoolApp.DTO;
+using SchoolApp.Models;
+
+namespace SchoolApp.Services
+{
+    public interface IUserService
+    {
+        Task<User?> VerifyAndGetUserAsync(UserLoginDTO credentials);
+
+        Task<UserReadOnlyDTO> GetUserByUsername(string username);
+
+        Task<PaginatedResult<UserReadOnlyDTO>> GetPaginatedUsersFilteredAsync(
+            int pageSize, UserFiltersDTO userFiltersDTO);
+    }
+}
