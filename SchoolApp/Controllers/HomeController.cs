6a220c1680fa706801f0af9cf5373f86e7114404
Timos <psychoson91@gmail.com>
2026-04-15 19:25:03 +0300

    chore(controllers):add controllers

--- SchoolApp/Controllers/HomeController.cs
+++ SchoolApp/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SchoolApp.Models;
 using System.Diagnostics;
@@ -6,17 +7,23 @@ namespace SchoolApp.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        [AllowAnonymous]
+        [HttpGet]
         public IActionResult Privacy()
         {
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [AllowAnonymous]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
